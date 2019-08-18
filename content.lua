98232a25f7e44f60a8fa72ef2c99f1d85e32a034
Chawan <chawan6@gmail.com>
2019-08-18 18:27:16 +0200

    Zone ids

--- content.lua
+++ content.lua
@@ -1,121 +1,54 @@
 function RPH_InitEnFactionGains()
 	local zone = {}
 	-- Kalimdor
-	zone.Mulgore = 7
-	zone.Darkshore = 62
-	zone.Thunder_Bluff = 88
-	zone.The_Exodar = 103
-	zone.Echo_Isles = 463
-	zone.Camp_Narache = 462
-	zone.Silithus = 81
-	zone.Felwood = 77
-	zone.Mount_Hyjal = 198
-	zone.Teldrassil = 57
-	zone.Moonglade = 80
-	zone.Azshara = 76
-	zone.Uldum = 249
-	zone.Durotar = 1
-	zone.Kalimdor = 12
-	zone.Orgrimmar = 85
-	zone.Desolace = 66
-	zone.Bloodmyst_Isle = 106
-	zone.Dustwallow_Marsh = 70
-	zone.UnGoro_Crater = 78
-	zone.Winterspring = 83
-	zone.Northern_Barrens = 10
-	zone.Valley_of_Trials = 461
-	zone.Ashenvale = 63
-	zone.Ammen_Vale = 468
-	zone.Stonetalon_Mountains = 65
-	zone.Feralas = 69
-	zone.Darnassus = 89
-	zone.Thousand_Needles = 64
-	zone.Southern_Barrens = 199
-	zone.AhnQiraj_The_Fallen_Kingdom = 327
-	zone.Azuremyst_Isle = 97
-	zone.Molten_Front = 338
-	zone.Shadowglen = 460
-	zone.Tanaris = 71
+	zone.Ashenvale = 1440
+	zone.Azshara = 1447
+	zone.Darkshore = 1439
+	zone.Darnassus = 1457
+	zone.Desolace = 1443
+	zone.Durotar = 1411
+	zone.Dustwallow_Marsh = 1445
+	zone.Felwood = 1448
+	zone.Feralas = 1444
+	zone.Moonglade = 1450
+	zone.Mulgore = 1412
+	zone.Orgrimmar = 1454
+	zone.Silithus = 1451
+	zone.Stonetalon_Mountains = 1442
+	zone.Tanaris = 1438
+	zone.Teldrassil = 1438
+	zone.The_Barrens = 1413
+	zone.Thousand_Needles = 1441
+	zone.Thunder_Bluff = 1456
+	zone.UnGoro_Crater = 1449
+	zone.Winterspring = 1452
 	
 	-- Eastern Kingdoms
-	zone.Wetlands = 56
-	zone.Blasted_Lands = 17
-	zone.Deathknell = 465
-	zone.Dun_Morogh = 27
-	zone.Tol_Barad_Peninsula = 245
-	zone.Silvermoon_City = 110
-	zone.Duskwood = 47
-	zone.Loch_Modan = 48
-	zone.Shimmering_Expanse = 205
-	zone.Arathi_Highlands = 14
-	zone.New_Tinkertown = 469
-	zone.Hillsbrad_Foothills = 25
-	zone.Undercity = 90
-	zone.Ghostlands = 95
-	zone.Eastern_Plaguelands = 23
-	zone.Gilneas = 179
-	zone.Burning_Steppes = 36
-	zone.The_Scarlet_Enclave = 124
-	zone.Eversong_Woods = 94
-	zone.Searing_Gorge = 32
-	zone.Tirisfal_Glades = 18
-	zone.Western_Plaguelands = 22
-	zone.Westfall = 52
-	zone.Coldridge_Valley = 427
-	zone.The_Cape_of_Stranglethorn = 210
-	zone.Vashjir = 203
-	zone.Kelpthar_Forest = 201
-	zone.Northshire = 425
-	zone.Gilneas_City = 202
-	zone.Eastern_Kingdoms = 13
-	zone.Twilight_Highlands = 241
-	zone.Redridge_Mountains = 49
-	zone.The_Hinterlands = 26
-	zone.Swamp_of_Sorrows = 51
-	zone.Silverpine_Forest = 21
-	zone.Abyssal_Depths = 204
-	zone.Sunstrider_Isle = 467
-	zone.Badlands = 15
-	zone.Ruins_of_Gilneas = 217
-	zone.Stormwind_City = 84
-	zone.Northern_Stranglethorn = 50
-	zone.Elwynn_Forest = 37
-	zone.Ruins_of_Gilneas_City = 218
-	zone.Stranglethorn_Vale = 224
-	zone.Isle_of_QuelDanas = 122
-	zone.Ironforge = 87
-	zone.Deadwind_Pass = 42
-	zone.Tol_Barad = 244
-
-	-- Battlegrounds
-	zone.Alterac_Valley = 91
-	zone.Warsong_Gulch = 92
-	zone.Arathi_Basin = 93
-	
-	-- Classic Dungeons
-	zone.Blackrock_Spire = 250
-	zone.The_Deadmines = 291
-	zone.Stratholme = 317
-	zone.Maraudon = 280
-	zone.Wailing_Caverns = 279
-	zone.Razorfen_Downs = 300
-	zone.Ragefire_Chasm = 213
-	zone.The_Temple_of_AtalHakkar = 220
-	zone.Razorfen_Kraul = 301
-	zone.ZulFarrak = 219
-	zone.Blackfathom_Deeps = 221
-	zone.Dire_Maul = 234
-	zone.The_Stockade = 225
-	zone.Uldaman = 230
-	zone.Blackrock_Depths = 242
-	zone.Shadowfang_Keep = 310
-	zone.Gnomeregan = 226
-	
-	-- Classic Raids
-	zone.Molten_Core = 232
-	zone.Blackwing_Lair = 287
-	zone.Ruins_of_AhnQiraj = 247
-	zone.Temple_of_AhnQiraj = 319
+	zone.Alterac_Mountains = 1416
+	zone.Arathi_Highlands = 1417
+	zone.Badlands = 1418
+	zone.Blasted_Lands = 1419
+	zone.Burning_Steppes = 1428
+	zone.Deadwind_Pass = 1430
+	zone.Dun_Morogh = 1426
+	zone.Duskwood = 1431
+	zone.Eastern_Plaguelands = 1423
+	zone.Elwynn_Forest = 1429
+	zone.Hillsbrad_Foothills = 1424
+	zone.Ironforge = 1455
+	zone.Loch_Modan = 1432
+	zone.Redrige_Mountains = 1433
+	zone.Searing_Gorge = 1427
+	zone.Silverpine_Forest = 1421
+	zone.Stormwind_City = 1453
+	zone.Stranglethorn_Vale = 1434
+	zone.Swamp_Of_Sorrows = 1435
+	zone.The_Hinterlands = 1425
+	zone.Tirisfal_Glades = 1420
+	zone.Undercity = 1458
+	zone.Western_Plaguelands = 1422
+	zone.Westfall = 1436
+	zone.Wetlands = 1437
 
 	if (RPH_IsAlliance) then
 		-- Stormwind
@@ -215,7 +148,6 @@ function RPH_InitEnFactionGains()
 	RPH_AddMob(87, 4, 8, "Blackwater Deckhand", 5)
 	RPH_AddMob(87, 4, 6, "Pirates at Faldir's Cove", "Arathi Highlands, Faldir's Cove")
 	RPH_AddMob(87, 4, 7, "Jazzrik and Rigglefuzz", 5, "Badlands")
-
 ---	local preGC = collectgarbage("count")
 	collectgarbage("collect")
 ---	print("Collected " .. (preGC-collectgarbage("count")) .. " kB of garbage RPH");
